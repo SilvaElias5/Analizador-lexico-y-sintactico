c7f0648327aeeb6429d4b9bd2b155adb75d63173
SilvaElias5 <102546260+SilvaElias5@users.noreply.github.com>
2022-12-13 22:26:23 -0600

    Create TipoToken.py

---
+++ TipoToken.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 17 17:04:56 2020
+
+@author: Jose Luis Quiroz Fabian
+@author: Adriana Perez Espinosa
+"""
+ 
+OPERADOR_ARITMETICO="operador_aritmetico"
+OPERADOR_RELACIONAL="operador_relacional"
+OPERADOR_LOGICO="operador logico"
+NUMERO="numero"
+SIGNO_DE_PUNTUACION="simbolo de puntuacion"
+ESPACIO="espacio en blanco"
+IDENTIFICADOR="identificador"
+PARENTESIS="parentesis"
+ASIGNACION="asignacion"
+CADENA="cadena"
+PALABRA_RESERVADA="palabra reservada"
+COMPARACION="comparacion"
+CORCHETES="corchetes"
+SIMBOLO="simbolo"
